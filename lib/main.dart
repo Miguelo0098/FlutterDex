209c9d987a87f7438bafe835d5106a421ecfd7b9
miguelo0098 <mugui098@gmail.com>
2019-05-22 20:44:02 +0200

    changed theme to white

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,9 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context){
     return MaterialApp(
       title: 'Startup Name Generator',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
       home: RandomWords(),
     );
   }
