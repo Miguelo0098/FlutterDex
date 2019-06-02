ef60da906802ea933212cd7bc0ff9a4b3e99fd02
miguelo0098 <mugui098@gmail.com>
2019-06-02 21:35:59 +0200

    remodelling app to fit a Pokedex app

--- lib/main.dart
+++ lib/main.dart
@@ -1,110 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:http/http.dart' as http;
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+    title: "Poke App",
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
 
-class MyApp extends StatelessWidget{
+class HomePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      title: 'Startup Name Generator',
-      theme: ThemeData(
-        primaryColor: Colors.white,
-      ),
-      home: RandomWords(),
-    );
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  var url = "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json"
+
+  @override
+  void initState() {
+    super.initState();
+
+    fetchData();
   }
-}
 
-class RandomWords extends StatefulWidget{
-  @override
-  RandomWordsState createState() => RandomWordsState();
-}
+  fetchData() async {
+    var res = http.get(url);
 
-class RandomWordsState extends State<RandomWords>{
-  final List<WordPair> _suggestions =<WordPair>[];
-  final Set<WordPair> _saved = Set<WordPair>();
-  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+    print(res);
+  }
+
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Startup Name Generator'),
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
-        ],
+        title: Text("Poke App"),
+        backgroundColor: Colors.red,
       ),
-      body: _buildSuggestions(),
-    );
-  }
 
-  Widget _buildSuggestions(){
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: /*1*/ (context, i){
-        if (i.isOdd) return Divider();  /*2*/ 
-        
-        final index = i ~/ 2; /*3*/
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-        }
-        return _buildRow(_suggestions[index]);
-      },
-    );
-  }
-
-  Widget _buildRow(WordPair pair){
-    final bool alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
+      body: Center(
+        child: Text("Hello From Pokemon App"),
       ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: (){
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          }else{
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
 
-  void _pushSaved(){
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (BuildContext context){
-          final Iterable<ListTile> tiles = _saved.map(
-            (WordPair pair){
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final List<Widget> divided = ListTile
-            .divideTiles(
-              context: context,
-              tiles: tiles,
-            )
-            .toList();
+      drawer: Drawer(),
 
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Saved Suggestions'),
-            ),
-            body: ListView(children: divided),
-          );
-        },
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        backgroundColor: Colors.red,
+        child: Icon(Icons.refresh),
       ),
     );
   }
